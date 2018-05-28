6a060236ccb4d94355d823588e788fad0320b2c9
Humbl3Stud3nt <waratjam@gmail.com>
2018-05-28 12:43:38 -0500

    Added stock_analytics module and accompanying test module

---
+++ stock_analytics.py
@@ -0,0 +1,18 @@
+"""
+Package dealing with comparative analysis and visualization of stock data
+using classes and functions defined in stock_data.py
+"""
+import stock_data
+import pylab
+
+# TODO: Learn how to use numpy and numpy arrays etc
+
+def plot(metric, *instrs, start_date, end_date):
+    pass
+
+def get_growth_rate(instr):
+    pass
+
+def get_fastest_growing(start_date, end_date, *instrs):
+    pass
+
---
+++ test_stock_analytics.py
@@ -0,0 +1,2 @@
+import unittest
+from stock_analytics import *
